77a0adbbce648b62a1397a55920db95acfcf3c67
Videonauth <videonauth@googlemail.com>
2016-07-08 07:35:23 +0200

    Added abc_list_generator.py and abc.wl file (issue #15)

--- abc_list_generator.py
+++ abc_list_generator.py
@@ -36,6 +36,25 @@ with open('abc.wl', 'a') as file:
         for b in range(len(natural)):
             if len(natural[b: b + a]) == a:
                 file.write(natural[b: b + a] + '\n')
-    # TODO: implementing reverse generation
+    reversed_lowercase = ascii_lowercase[:: -1]
+    reversed_uppercase = ascii_uppercase[:: -1]
+    reversed_digits = digits[:: -1]
+    reversed_natural = natural[:: -1]
+    for a in range(3, len(reversed_lowercase) + 1):
+        for b in range(len(reversed_lowercase)):
+            if len(reversed_lowercase[b: b + a]) == a:
+                file.write(reversed_lowercase[b: b + a] + '\n')
+    for a in range(3, len(reversed_uppercase) + 1):
+        for b in range(len(reversed_uppercase)):
+            if len(reversed_uppercase[b: b + a]) == a:
+                file.write(reversed_uppercase[b: b + a] + '\n')
+    for a in range(3, len(reversed_digits) + 1):
+        for b in range(len(reversed_digits)):
+            if len(reversed_digits[b: b + a]) == a:
+                file.write(reversed_digits[b: b + a] + '\n')
+    for a in range(3, len(reversed_natural) + 1):
+        for b in range(len(reversed_natural)):
+            if len(reversed_natural[b: b + a]) == a:
+                file.write(reversed_natural[b: b + a] + '\n')
 
 file.close()
