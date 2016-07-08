9c26ffa824b2557fefbdb25f676974c71985e3fd
Videonauth <videonauth@googlemail.com>
2016-07-09 00:38:04 +0200

    First draft of setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='passgen',
+    version='0.0.8',
+    packages=['', '', ''],
+    package_dir={'': 'passgen'},
+    url='https://github.com/Videonauth/passgen',
+    license='See LICENSE file',
+    author='Videonauth',
+    author_email='videonauth@googlemail.com',
+    description='A simple password generator'
+)
