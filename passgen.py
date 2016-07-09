c7f34525d87e82a7fbec3ac3fa005e6c71de0937
Videonauth <videonauth@googlemail.com>
2016-07-09 04:20:47 +0200

    A version jump

--- passgen.py
+++ passgen.py
@@ -24,7 +24,7 @@ import random
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 import textwrap
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 # Define the characters which are valid for making a password
 char_all = {"d": digits, "l": ascii_lowercase, "u": ascii_uppercase, "p": punctuation, "s": " "}
